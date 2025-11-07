299cae0880a86efd07cf12ef2144157366b8cdea
victor stone <victor.stone@gmail.com>
2025-11-07 17:58:59 -0300

    Update fetch credentials to 'include' for cross-origin requests in clientAPI.js

--- public/assets/js/clientAPI.js
+++ public/assets/js/clientAPI.js
@@ -32,7 +32,7 @@ function server() {
     const response = await fetch(url, {
       ...options,
       headers,
-      credentials: true
+      credentials: 'include'
     });
     
     // Parse the response based on the parseAsJson flag
